891afd195844800373dedb3580cec512ef9aa023
Parsla Bogdanova <your@email.ex>
2024-10-23 12:43:59 +0300

    display all posts

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+namespace App\Http\Controllers;
+use App\Models\Note;
+
 class NoteController extends Controller
 {
-    //
+
+    public function index(){
+
+        $notes = Note::all();
+
+
+        return view('notes.index', ['allNotes' => $notes]);
+    }
 }
